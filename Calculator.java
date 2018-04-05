c63c270fb1aa8fb6933d13fcd6d39855384eedf4
LeeSoYun <thdbs4380@naver.com>
2018-04-05 19:08:04 +0900

    homework2

---
+++ Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+    public static void main(String...args){
+        System.out.printf("%d + %d= %d\n%d - %d= %d\n%d * %d= %d",1,2,1+2, 1,2,1-2,1,2,1*2 );
+    }
+}
---
+++ Triangle.java
@@ -0,0 +1,5 @@
+public class Triangle {
+    public static void main(String...args){
+        System.out.printf("*\n**\n***\n****\n*****\n******");
+    }
+}
